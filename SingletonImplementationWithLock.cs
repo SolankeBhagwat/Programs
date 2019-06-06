ab40f202c4e478a2a62045e825d56ba31f74189b
Bhagwat Solanke <51320189+SolankeBhagwat@users.noreply.github.com>
2019-06-06 12:24:57 +0530

    Create SingletonImplementationWithLock.cs

---
+++ SingletonImplementationWithLock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public sealed class Logger
+{
+    private static Logger _logger = null;
+    private Logger()
+    {
+    }
+    private static object obj = null;
+    public static Logger GetLogger()
+    {
+        lock (obj)
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+        }
+
+        return _logger;
+    }
+
+}
